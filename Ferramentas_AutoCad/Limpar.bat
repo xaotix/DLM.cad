95aa5c79f2d55c8925e542e94fb881fdfaa2698d
Daniel Lins Maciel <dan.8p@hotmail.com>
2023-07-27 10:29:05 -0300

    Ajustado problemas com debug

--- Ferramentas_AutoCad/Limpar.bat
+++ Ferramentas_AutoCad/Limpar.bat
@@ -1,3 +1,4 @@
 ﻿taskkill /f /im acad.exe
 xcopy /K /H /Y /S "*.dll" "C:\Program Files\Autodesk\AutoCAD Mechanical 2012\"
 xcopy /K /H /Y /S "*.scr" "C:\Program Files\Autodesk\AutoCAD Mechanical 2012\"
+pause
