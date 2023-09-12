07e266135e49190799a02a4bcbd9c8a1e81a91c2
Daniel Lins Maciel <dan.8p@hotmail.com>
2023-09-12 17:06:25 -0300

    Ajustes

--- Ferramentas_AutoCad/Menus/Menu_Bloco_Peca.xaml.cs
+++ Ferramentas_AutoCad/Menus/Menu_Bloco_Peca.xaml.cs
@@ -387,7 +387,7 @@ namespace DLM.cad.Menus
             else if (tipo_selecionado == Cfg.Init.CAD_ATT_RMT && rmt_sel != null)
             {
                 var mm = new Conexoes.RMT(rmt_sel, DBases.GetBobinaDummy(Cfg.Init.Material_RMT));
-                mm.Qtd = (int)qtd;
+                mm.Quantidade = (int)qtd;
                 id = mm.id_telha;
                 txt_codigo.Text = mm.NomeFim;
             }
